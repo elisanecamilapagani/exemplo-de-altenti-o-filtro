00636ce486c66b95f78e1246ac1211e7e20437cc
Elisane Pagani <elisanecamila20@gmail.com>
2020-06-30 17:09:06 -0400

    Trabalho Finalizado

---
+++ autenticacao-filtro/exemplo-autenticacao-filtro/src/routes/paginas.js
@@ -0,0 +1,67 @@
+import {
+    Router
+} from 'express';
+
+import LoginController from '../controller/LoginController';
+
+const router = Router();
+const loginCtrl = new LoginController();
+
+/**
+ * Rota da página de login
+ */
+router.get('/', (req, res) => res.render('login'));
+
+//rotas para cadastrar um novo usuario
+router.get('/cadastrar', loginCtrl.verificarAutenticacao, (req, res) =>
+    res.render('cadastrar', { usuario: req.session.usuario }));
+
+router.post('/cadastrar', (req, res) => {
+
+    const { email, nome, senha } = req.body;
+    const resposta1 = loginCtrl.registrarUsuario(email, nome, senha);
+    res.render('cadastrar', { mensagem: resposta1.mensagem, usuario: req.session.usuario });
+});
+
+
+/**
+ * Rota para a verificação do login do usuário
+ */
+router.post('/', (req, res) => {
+    /**
+     * Pega o e-mail e senha que o usuário 
+     * informou no formulário
+     */
+    const { email, senha } = req.body;
+    const resposta = loginCtrl.realizarLogin(email, senha);
+    /**
+     * Se o usuário foi autenticado, adiciona o
+     * token JWT na sessão para que ele possa ser
+     * usado em toda a requisição para verificar
+     * se o usuário está autenticado e se a sessão
+     * ainda é válida.
+     */
+    if (resposta.autenticado) {
+        req.session.token = resposta.token;
+        req.session.usuario = resposta.usuario;
+        res.redirect('/home');
+    } else {
+        res.render('login', { mensagem: resposta.mensagem });
+    }
+});
+
+
+
+/**
+ * Rota para a página Home do sistema, que é protegida por autenticação.
+ * Ela só será acessível se o método verificarToken identificar que o
+ * token do usuário não expirou, ou seja, se ainda não se passou 1 hora
+ * desde que ele realizou o seu login.
+ */
+router.get('/home', loginCtrl.verificarToken, (req, res) =>
+    res.render('home', { usuario: req.session.usuario }));
+
+//rota para botão Logout
+router.get('/botaoLogout', loginCtrl.logout);
+
+export default router;
